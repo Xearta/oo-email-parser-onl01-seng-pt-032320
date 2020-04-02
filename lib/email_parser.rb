a510848f03bfa6f0791456a109409d194186be4e
Tanner Townsend <rsmcwow@gmail.com>
2020-04-02 03:45:28 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailAddressParser
   end
   
   def parse
-    @email_addresses
+    @email_addresses = @emails
     #@email_addresses = @emails.split(", ")
     #@email_addresses.collect {|email| email.strip}
     
